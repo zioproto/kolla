9fc2d2b917f0f31a5925bb758717cf2f7091b5cc
Saverio Proto <saverio.proto@switch.ch>
2017-01-31 10:49:06 +0100

    Add SNAP support when finding Docker images folder
    Change-Id: I046a93de72666eae11cb88eb4577c97935f72520

--- kolla/image/build.py
+++ kolla/image/build.py
@@ -536,10 +536,17 @@ class KollaWorker(object):
         self.maintainer = conf.maintainer
 
     def _get_images_dir(self):
-        possible_paths = (
-            PROJECT_ROOT,
-            os.path.join(sys.prefix, 'share/kolla'),
-            os.path.join(sys.prefix, 'local/share/kolla'))
+        if os.environ.get('SNAP'):
+            possible_paths = (
+                PROJECT_ROOT,
+                os.path.join(sys.prefix, 'share/kolla'),
+                os.path.join(sys.prefix, 'local/share/kolla'),
+                os.path.join(os.environ.get('SNAP'), 'share/kolla'))
+        else:
+            possible_paths = (
+                PROJECT_ROOT,
+                os.path.join(sys.prefix, 'share/kolla'),
+                os.path.join(sys.prefix, 'local/share/kolla'))
 
         for path in possible_paths:
             image_path = os.path.join(path, 'docker')
